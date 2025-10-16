3c88ffc4d68e24d5d81f87e47d501770edfde864
ChitranshBP <chitransh@brandingpioneers.co.in>
2025-10-16 18:52:49 +0530

    locaytion removed from doctor page

--- src/pages/DoctorProfile.jsx
+++ src/pages/DoctorProfile.jsx
@@ -1851,12 +1851,12 @@ const DoctorProfile = () => {
                   {doctor.experience} Experience
                 </span>
               )}
-              {doctor.contact?.address && (
+              {/* {doctor.contact?.address && (
                 <span className="flex items-center">
                   <SafeIcon icon={FiMapPin} className="w-5 h-5 mr-2" />
                   {doctor.contact.address}
                 </span>
-              )}
+              )} */}
             </div>
 
             <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
