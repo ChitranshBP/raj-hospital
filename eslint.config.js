2085f8665c298b570570e31239edfd5aa7fcfb1e
ChitranshBP <chitransh@brandingpioneers.co.in>
2025-12-05 13:44:52 +0530

    bug fixed

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-{ ignores: ['dist'] },
+{ ignores: ['dist', 'verify-doctor-links.mjs'] },
 js.configs.recommended,
 {
-  files: ['**/*.{js,jsx}'],
+  files: ['**/*.{js,jsx,mjs}'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: {
